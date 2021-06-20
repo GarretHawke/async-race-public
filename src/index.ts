7ef7d8c814f34e705fd6685ea7c821c3dd164fa5
GarretHawke <alexandra17au@gmail.com>
2021-06-20 14:21:53 +0300

    feat: add start

--- src/index.ts
+++ src/index.ts
@@ -31,14 +31,14 @@ class App extends Component {
 
     this.pageGarage = new PageGarage(this.element);
 
-    let text = new Component(this.pageGarage.element, 'span');
+    /* let text = new Component(this.pageGarage.element, 'span');
     text.element.innerText = `
     Привет! Это задание оказалось довольно сложным для меня, поэтому у меня уходит ооочень много времени
     на его разбор и решение. Ко времени дедлайна я еще не успела реализовать все фичи. Буду благодарна, если ты мне дашь еще немного времени
     и проверишь в последний день. Я очень стараюсь, и надеюсь, что еще парочка дней помогут мне добавить еще
     функционала в это приложение. Ну а если не захочешь - это твое решение, все равно спасибо!) Я в первую очередь
     здесь за знаниями, а не получением баллов любой ценой.
-    С уважением, Garret Hawke`;
+    С уважением, Garret Hawke`; */
     this.pageWinners = new PageWinners(this.element);
 
     this.eventObserver = new EventObserver();
